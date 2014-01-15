bf5f2657b8b94c6a268d94f4f5f4a003530ba2c2
Iain MacDonald <IJMacD@gmail.com>
2014-01-15 19:06:44 +0800

    Added proxy

---
+++ proxy.php
@@ -0,0 +1,19 @@
+<?php
+define('BASE_URL', "http://192.168.0.138/studio");
+$request_uri = $_SERVER['REQUEST_URI'];
+$script_name = $_SERVER['SCRIPT_NAME'];
+if(strpos($request_uri,$script_name)===0){
+	$path = substr($request_uri,strlen($script_name));
+	$url = BASE_URL . $path;
+	$options = array(
+		'http' => array(
+			'method'  => 'POST',
+			'content' => http_build_query($_POST),
+		),
+	);
+	$context  = stream_context_create($options);
+	$result = file_get_contents($url, false, $context);
+
+	echo $result;
+}
+?>
