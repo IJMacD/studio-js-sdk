bca8accc90dfa51b95c31310487a82af364415e7
Iain MacDonald <IJMacD@gmail.com>
2014-02-21 18:40:45 +0800

    Added event locations and text colour changing

--- calendar.js
+++ calendar.js
@@ -16,7 +16,8 @@
 				$.each(data.CalendarCourse, function(i,item){
 					var start = new Date(item.ScheduleDate),
 						end = new Date(item.ScheduleDate),
-						tutor = iL.findTutor(item.Tutor);
+						tutor = iL.findTutor(item.Tutor),
+						textColour = (tutor && (colourLightness(tutor.colour) < 128)) ? "#fff" : "#333";
 					start.setHours(item.Starttime.substr(0,2));
 					start.setMinutes(item.Starttime.substr(2,2));
 					end.setHours(item.endtime.substr(0,2));
@@ -25,7 +26,10 @@
 						title: item.Coursetitle,
 						start: start,
 						end: end,
-						color: tutor && tutor.colour
+						color: tutor && tutor.colour,
+						tutor: item.Tutor,
+						resourceId: item.Location,
+						textColor: textColour
 					});
 				});
 				callback(events);
@@ -33,5 +37,15 @@
 			"json");
 	}
 	Calendar.getLessons = getLessons;
+
+	function colourLightness(hex){
+		var c = hex.substring(1),      // strip #
+			rgb = parseInt(c, 16),   // convert rrggbb to decimal
+			r = (rgb >> 16) & 0xff,  // extract red
+			g = (rgb >>  8) & 0xff,  // extract green
+			b = (rgb >>  0) & 0xff;  // extract blue
+
+		return (0.2126 * r + 0.7152 * g + 0.0722 * b); // per ITU-R BT.709
+	}
 
 }(window));
