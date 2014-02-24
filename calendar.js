01636692eb3d28d2ac5e7e58c9d57133112c801e
Iain MacDonald <IJMacD@gmail.com>
2014-02-24 12:45:26 +0800

    Moved presentation code out of SDK

--- calendar.js
+++ calendar.js
@@ -7,10 +7,11 @@
 	window.iL = iLearner;
 	iLearner.Calendar = Calendar;
 
-	function getLessons(start, end, callback){
+	function getLessons(start, end){
 		var post_data = {
-			sDate: iL.formatDate(start)
-		};
+				sDate: iL.formatDate(start)
+			},
+			deferred = $.Deferred();
 		$.post(iL.API_ROOT + 'process_getCalendarData.php',
 			post_data,
 			function(data){
@@ -18,8 +19,7 @@
 				$.each(data.CalendarCourse, function(i,item){
 					var start = new Date(item.ScheduleDate),
 						end = new Date(item.ScheduleDate),
-						tutor = iL.findTutor(item.Tutor),
-						textColour = (tutor && (colourLightness(tutor.colour) < 128)) ? "#fff" : "#333",
+						tutor = iL.findTutor(item.Tutor, true),
 						level = item.Coursetitle.match(levelRegex),
 						course = item.Coursetitle.replace(levelRegex, "");
 					start.setHours(item.Starttime.substr(0,2));
@@ -31,28 +31,17 @@
 						title: item.Coursetitle,
 						start: start,
 						end: end,
-						color: tutor && tutor.colour,
-						tutor: item.Tutor,
-						resourceId: item.Location,
-						textColor: textColour,
+						tutor: tutor,
+						room: item.Location,
 						course: course,
 						level: level
 					});
 				});
-				callback(events);
+				deferred.resolve(events);
 			},
 			"json");
+		return deferred.promise();
 	}
 	Calendar.getLessons = getLessons;
-
-	function colourLightness(hex){
-		var c = hex.substring(1),    // strip #
-			rgb = parseInt(c, 16),   // convert rrggbb to integer
-			r = (rgb >> 16) & 0xff,  // extract red
-			g = (rgb >>  8) & 0xff,  // extract green
-			b = (rgb >>  0) & 0xff;  // extract blue
-
-		return (0.2126 * r + 0.7152 * g + 0.0722 * b); // per ITU-R BT.709
-	}
 
 }(window));
