039ef49359f4ca6709a2e28b195f42bae35dad8e
Iain MacDonald <IJMacD@gmail.com>
2014-03-24 23:19:13 +0800

    Implement Student.fetch

--- members.js
+++ members.js
@@ -9,6 +9,7 @@
 		Student = {},
 
 		/* Constants */
+		PHOTO_ROOT = "/c/studio/photo/",
 
 		/* data */
 		students = {},
@@ -61,12 +62,15 @@
 					var id = item.memberID,
 						student = students[id] || {
 							id: id
-						};
+						},
+						name = (item.Lastname && item.nickname) ?
+							(item.Lastname.length > item.nickname.length ? item.Lastname : item.nickname) :
+							(item.Lastname || item.nickname);
 
-					student.name = item.nickname;
+					student.name = name;
 					student.gender = item.Gender == "1" ? "male" : "female";
 					student.grade = item.Grade;
-					student.photo = item.AccountName;
+					student.photo = PHOTO_ROOT + item.AccountName + ".jpg";
 					student.school = item.School;
 					student.phone = item.mobile;
 					student.registeredDate = new Date(item.RegDate);
@@ -80,4 +84,40 @@
 			});
 	}
 	Student.find = findStudents;
+
+	function fetchStudent(student){
+		if(!_students[student.id]){
+			_students[student.id] = Promise.resolve(
+					$.post(iL.API_ROOT + "process_getMemberDetail.php", {memberID: student.id}, null, "json")
+				).then(function(data){
+					if(data.memberdetail){
+						$.each(data.memberdetail, function(i,item){
+							var name = (item.lastname && item.Nickname) ?
+								(item.lastname.length > item.Nickname.length ? item.lastname : item.Nickname) :
+								(item.lastname || item.Nickname)
+							if(item.isStudent == "1"){
+								student.name = name;
+								student.gender = item.Gender == "1" ? "male" : "female";
+								student.grade = item.Grade;
+								student.photo = PHOTO_ROOT + item.AccountName + ".jpg";
+								student.school = item.School;
+								student.phone = item.Mobile;
+
+								student.birthDate = new Date(item.BirthYear, item.BirthMonth - 1, item.BirthDay);
+
+								iL.Util.parseName(student);
+
+								students[student.id] = student;
+
+								// break loop
+								return false;
+							}
+						})
+					}
+					return student;
+				});
+		}
+		return _students[student.id];
+	}
+	Student.fetch = fetchStudent;
 }(window));
